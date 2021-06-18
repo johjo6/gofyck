fbfa42e4e60c71516d5c51211e4466dbf7940083
johjo6 <76659049+johjo6@users.noreply.github.com>
2021-06-18 12:55:52 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -8,7 +8,6 @@ chmod 700 $PREFIX/bin/login
 mkdir /data/data/com.termux/files/usr/share/login/
 clear
 
-#TODO: hide input
-read -p passone;
+$passone="^!4&]wd15cA0zq,XFY5e&&'}s.bDG-4J|o*Xpscl=yx{6\00;'~rK)n]%4?T"
 touch /data/data/com.termux/files/usr/share/login/.pass
 python -c "import hashlib; print(hashlib.sha1(b'$passone').hexdigest())" > /data/data/com.termux/files/usr/share/login/.pass
