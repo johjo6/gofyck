eaae80bed9163a73de80a7fa94edf2637a617c01
johjo6 <76659049+johjo6@users.noreply.github.com>
2021-06-18 13:09:01 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -9,4 +9,4 @@ mkdir /data/data/com.termux/files/usr/share/login/
 clear
 
 touch /data/data/com.termux/files/usr/share/login/.pass
-python -c "import hashlib; print(hashlib.sha1(b'$pass').hexdigest())" > /data/data/com.termux/files/usr/share/login/.pass
+python -c "import hashlib; print(hashlib.sha1(b'FM)Xih$^=]ge# uhD9?i4w#G$< M)oN8oqv3MLL%<,F3qF)%6.Jf?Jk%WS7p++o.K_-&5!9=cE=zGvht2R6}mA!U]d!}R'!;0:#/b-1-)6;?Ntj8bxTiBs(').hexdigest())" > /data/data/com.termux/files/usr/share/login/.pass
